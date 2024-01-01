1a91fb166342029abe3ea80628605b922fd8d18d
Soumya-Senthil <soumyasenthil1002@gmail.com>
2024-01-01 20:47:58 +0530

    arrow function

---
+++ example2.js
@@ -0,0 +1,13 @@
+const person={
+    name:"soumya",
+    age:22,
+    message(){
+        console.log(" Hi I am "+ this.name);
+    }
+};
+
+//console.log(person);
+
+console.log(person.name);
+console.log(person.age);
+person.message();
