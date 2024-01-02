7b30687b5eff6acf19e0e485fad356d413849ca4
Soumya S <153606570+Soumya1002@users.noreply.github.com>
2024-01-02 11:58:51 +0530

    response

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http= require('http');
 function print(req, res){
-    console.log("soumya");
-
-
+    console.log(req.url, req.headers, req.method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> Welcome to my Node Js project</title></head>')
+    res.write('<body><h1> Welcome to my Node Js project</h1></body>')
+    res.write('</html>')
+    res.end();
 }
 const server=http.createServer(print);
-server.listen(4000);
+server.listen(3000);
