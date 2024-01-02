4b3ac5a2c27e41384d13de86f1f19765f5a84b20
Soumya S <153606570+Soumya1002@users.noreply.github.com>
2024-01-02 12:26:10 +0530

    simple form that writes to a file and redirects to a page

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
-const http= require('http');
-function print(req, res){
-    const url=req.url;
+const http = require('http');
+const fs = require('fs');
 
-    if(url==='/home'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> Welcome to my Node Js project</title></head>')
-        res.write('<body><h1> Welcome Home</h1></body>')
-        res.write('</html>')
-        res.end();
-       
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form id="msg" action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+});
 
-    }
-
-    else if(url==='/about'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> Welcome to my Node Js project</title></head>')
-        res.write('<body><h1> Welcome to about us page</h1></body>')
-        res.write('</html>')
-        res.end();
-
-    }
-
-    else if (url==='/node'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> Welcome to my Node Js project</title></head>')
-        res.write('<body><h1> Welcome to my Node JS project</h1></body>')
-        res.write('</html>')
-        res.end();
-
-    }
-   
-}
-const server=http.createServer(print);
 server.listen(3000);
