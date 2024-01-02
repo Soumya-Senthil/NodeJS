2b2549b791a58a246941ca969d91d5a00117f211
Soumya S <153606570+Soumya1002@users.noreply.github.com>
2024-01-02 12:09:36 +0530

    custom response based on url

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const http= require('http');
 function print(req, res){
-    console.log(req.url, req.headers, req.method);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> Welcome to my Node Js project</title></head>')
-    res.write('<body><h1> Welcome to my Node Js project</h1></body>')
-    res.write('</html>')
-    res.end();
+    const url=req.url;
+
+    if(url==='/home'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> Welcome to my Node Js project</title></head>')
+        res.write('<body><h1> Welcome Home</h1></body>')
+        res.write('</html>')
+        res.end();
+       
+
+    }
+
+    else if(url==='/about'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> Welcome to my Node Js project</title></head>')
+        res.write('<body><h1> Welcome to about us page</h1></body>')
+        res.write('</html>')
+        res.end();
+
+    }
+
+    else if (url==='/node'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> Welcome to my Node Js project</title></head>')
+        res.write('<body><h1> Welcome to my Node JS project</h1></body>')
+        res.write('</html>')
+        res.end();
+
+    }
+   
 }
 const server=http.createServer(print);
 server.listen(3000);
